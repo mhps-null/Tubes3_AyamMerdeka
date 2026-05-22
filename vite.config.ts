343bcf289f89325d93a0957b83fea48abf6854af
mhps-null <mharisputras.work@gmail.com>
2026-05-22 20:56:16 +0700

    init repo extension developer

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        content: resolve("src/content.ts")
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name].js",
+        assetFileNames: "assets/[name][extname]"
+      }
+    }
+  }
+});
